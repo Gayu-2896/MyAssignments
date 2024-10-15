e2e9014c2dae7cac3a77a87d3800afc04497994b
Gayu-2896 <gayathrijayasanker28@gmail.com>
2024-10-16 02:37:42 +0530

    Add files via upload
    Day2

---
+++ day1/day2/LearnEvenNumbers.java
@@ -0,0 +1,17 @@
+package week1.day2;
+
+public class LearnEvenNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int maxRange =10;
+		for(int i=1;i <= maxRange ; i++)
+		{
+			if(i%2==0)
+			{
+				System.out.println(i + " is Even Number");
+			}
+		}
+	}
+}
